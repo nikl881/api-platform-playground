327e9586dedea7b2417fe25a5ea2e45cd785cf89
nikl881 <info@findniels.com>
2021-01-20 10:25:07 +0100

    :construction: WIP fetch product and offer list from endpoint

--- clientApp/src/getData.js
+++ clientApp/src/getData.js
@@ -1,8 +1,13 @@
 // getData file
+import { baseURL } from './config.js';
+import { show } from  './view.js';
 
-// current local address of backend app
-export const getData = () =>  {
-    axios.get('http://localhost:8000/api/products').then(function (response) {
-        console.log(response)
-    })
+export const getData = () => {
+    axios.get( baseURL + '/api/products' ).then(function (response) {
+            // console.log(response)
+            show(response.data['hydra:member']);
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
 }
