81b7c3552e4dd15d4ba834a64ca9ea0032014641
nikl881 <info@findniels.com>
2021-01-21 09:22:55 +0100

    :recycle: refactor routing for client-app

--- clientApp/src/getData.js
+++ clientApp/src/getData.js
@@ -2,15 +2,11 @@
 import {baseURL} from './config.js';
 import {show} from './view.js';
 import {pagination} from "./pagination.js";
+import {router} from "./router.js";
 
 export const getData = (event) => {
 
-    if (typeof event.target.nextLink !== 'undefined')
-        var url = event.target.nextLink
-    else
-        var url = '/api/products'
-
-    axios.get(baseURL + url).then(function (response) {
+    axios.get(baseURL + router(event)).then(function (response) {
         console.log(response)
         pagination(response.data);
         show(response.data['hydra:member']);
--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
