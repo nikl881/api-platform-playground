8335f99230a36e98ff8b7460523b77790c1faba6
nikl881 <info@findniels.com>
2021-01-29 15:47:28 +0100

    post rest api request for adding offers

---
+++ clientBackend/app.js
@@ -0,0 +1,3 @@
+import {Products} from "./src/actions/Products.js";
+
+new Products
---
+++ clientBackend/src/actions/Products.js
@@ -0,0 +1,27 @@
+import {baseURL} from "../config.js";
+
+
+export class Products {
+    constructor() {
+        axios.get(baseURL + '/api/products?pagination=false')
+            .then(response => {
+                // console.log(response.data['hydra:member']);
+                response.data['hydra:member'].forEach(product => {
+                    this.addOptionElement(product.name, product.id)
+                } )
+            })
+            .catch(function (error){
+                console.log(error);
+            })
+    }
+
+    addOptionElement(text, value)
+    {
+        let option = document.createElement("option");
+        option.text = text;
+        option.value = value;
+        let select = document.getElementById("product");
+        select.appendChild(option);
+    }
+
+}
